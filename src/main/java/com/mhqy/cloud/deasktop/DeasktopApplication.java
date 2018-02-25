581d8b3f3808768d59938db40868490a25353333
kunaimeng <1272768836@qq.com>
2018-02-25 18:26:04 +0800

    初始化项目

---
+++ src/main/java/com/mhqy/cloud/deasktop/DeasktopApplication.java
@@ -0,0 +1,12 @@
+package com.mhqy.cloud.deasktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeasktopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeasktopApplication.class, args);
+	}
+}
---
+++ src/main/java/com/mhqy/cloud/deasktop/controller/DesktopController/IndexController.java
@@ -0,0 +1,19 @@
+package com.mhqy.cloud.deasktop.controller.DesktopController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    private final static Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping("index")
+    public String index(){
+        logger.info("进来了");
+        return "login";
+    }
+
+}
