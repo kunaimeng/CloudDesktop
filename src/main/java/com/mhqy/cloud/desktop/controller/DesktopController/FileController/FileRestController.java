eb4f8a21b344d9e9ac1af94b68aa7314cb576804
kunaimeng <1272768836@qq.com>
2018-03-04 11:49:23 +0800

    添加controller

---
+++ src/main/java/com/mhqy/cloud/desktop/controller/DesktopController/FileController/FileRestController.java
@@ -0,0 +1,11 @@
+package com.mhqy.cloud.desktop.controller.DesktopController.FileController;
+
+/**
+ * @Description:文件ajax请求
+ * @author: peiqiankun
+ * @date: 2018/3/3
+ * @mail: peiqiankun@jd.com
+ * @version: v1.0
+ */
+public class FileRestController {
+}
